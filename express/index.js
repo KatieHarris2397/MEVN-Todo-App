e9010a26d66b57bcb59ed7ac38f04d0cf590990e
Katie Harris <katie.harris2397@gmail.com>
2024-11-21 15:22:51 -0500

    First commit

---
+++ express/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+require('dotenv').config();
+
+const TodoListRoutes = require('./routes/api/todoList')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const PORT = 3000
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
+app.use('/api/todoList', TodoListRoutes)
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`)
+})
